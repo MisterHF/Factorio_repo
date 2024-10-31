0af22b8c5048ad144e60a1a210d135aece50476d
MisterHF <h.florianpro@gmail.com>
2024-10-31 18:27:32 +0100

    Début scripte pour systeme de craft et UI

---
+++ Assets/Script/Craft/CraftingController.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingController : MonoBehaviour
+{
+    [SerializeField] private GameObject craftPanel; //UI
+    [SerializeField] GameObject content; // Ressource entrée
+
+    private List<CraftingController> craft;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/ScriptableObject/CraftingRule.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CraftingRule", menuName = "Scriptable Objects/CraftingRule")]
+public class CraftingRule : ScriptableObject
+{
+    public ItemData result;
+    public List <ItemData> requires = new();
+}
