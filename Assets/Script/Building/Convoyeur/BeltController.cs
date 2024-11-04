29afd070ccb7b22bf09c83b0d3a4e6556c3416b6
Hibiz3a <m.raynaud@lahorde.school>
2024-11-04 17:31:23 +0100

    SetUp

---
+++ Assets/Script/Building/Convoyeur/BeltController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BeltController : MonoBehaviour
+{
+    [SerializeField] private float BeltSpeed;
+    [SerializeField] private ItemData TransportedItem;
+
+    
+    private void Update()
+    {
+        
+    }
+    
+}
