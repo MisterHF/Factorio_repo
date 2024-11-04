bbdb3c80f0f5ceb7fbcf6f28235a7493d03b27e9
MisterHF <h.florianpro@gmail.com>
2024-11-04 15:55:01 +0100

    Add UI and Script for crafting

--- Assets/Script/Craft/CraftingController.cs
+++ Assets/Script/Craft/CraftingController.cs
@@ -1,18 +1,27 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CraftingController : MonoBehaviour
 {
     [SerializeField] private GameObject craftPanel; //UI
-    [SerializeField] GameObject content; // Ressource entrée
+    [SerializeField] Transform content; // Ressource entrée
 
-    private List<CraftingController> craft;
+    [SerializeField] private List<CraftingRule> craft;
+    [SerializeField] private GameObject buttonPrefab;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < craft.Count; i++) 
+        {
+            GameObject btn =  Instantiate(buttonPrefab, content);
+            btn.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = craft[i].result.name;
+            btn.transform.GetChild(1).GetComponent<Image>().sprite = craft[i].result.sprite;
+            btn.GetComponent<SpawnRequireSlots>().RequireSlot1 = craft[i];
+        }
     }
 
     // Update is called once per frame
