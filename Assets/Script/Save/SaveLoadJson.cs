4c17e7e6f7872619608ea171cc438790101b865f
MisterHF <h.florianpro@gmail.com>
2024-11-08 12:22:20 +0100

    Fix script save and load

--- Assets/Script/Save/SaveLoadJson.cs
+++ Assets/Script/Save/SaveLoadJson.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 
 public class SaveLoadJson : MonoBehaviour
@@ -19,7 +20,7 @@ public class SaveLoadJson : MonoBehaviour
         {
             Instance = this;
             _gameData = new GameData();
-            _saveFilePath = Application.dataPath + "/GameData.json";
+            _saveFilePath = Application.persistentDataPath + "/PlayerData.json";
             DontDestroyOnLoad(gameObject);
         }
         else
@@ -27,4 +28,43 @@ public class SaveLoadJson : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SaveToJson();
+        }
+
+    }
+
+    public void SaveToJson()
+    {
+
+        string savePlayerData = JsonUtility.ToJson(_gameData);
+        File.WriteAllText(_saveFilePath, savePlayerData);
+
+        Debug.Log("Save file created at: " + _saveFilePath);
+
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(_saveFilePath))
+        {
+            string loadPlayerData = File.ReadAllText(_saveFilePath);
+            _gameData = JsonUtility.FromJson<GameData>(loadPlayerData);
+        }
+    }
+
+    //public void RegisterInformations(bool alteratePlayerStats, float speed, int mining_range, int mining_speed)
+    //{
+    //    if (alteratePlayerStats)
+    //    {
+    //        _playerSpeed = speed;
+    //        _playerMiningSpeed = mining_speed;
+    //        _playerMiningRange = mining_range;
+    //    }
+
+    //}
 }
