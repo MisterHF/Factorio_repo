b5d2ecc115dbfd354f7c7b7eb97e6cddf29782bb
Hibiz3a <m.raynaud@lahorde.school>
2024-11-13 17:35:10 +0100

    Ui change
    Ui change on :
    - Furnace
    - Crafter
    - Slider
    - Button
    - Inventory
    - Slot

--- Assets/Script/Building/BuildingHover.cs
+++ Assets/Script/Building/BuildingHover.cs
@@ -1,9 +1,11 @@
+using System;
 using UnityEngine;
 
 public class BuildingHover : MonoBehaviour
 {
     private SpriteRenderer haloRenderer;
     private GameObject infoPanel;
+    private Collider2D hitColliders;
 
     private void Start()
     {
@@ -17,26 +19,28 @@ public class BuildingHover : MonoBehaviour
     private void Update()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Collider2D[] hitColliders = Physics2D.OverlapPointAll(mousePos);
+        hitColliders = Physics2D.OverlapPoint(mousePos);
 
         bool isHovering = false;
-
-        foreach (var hitCollider in hitColliders)
+        if(hitColliders != null)
         {
-            if (hitCollider.gameObject == gameObject)
+            if (hitColliders.gameObject == gameObject)
             {
                 haloRenderer.enabled = true;
-                infoPanel.transform.GetChild(0).gameObject.SetActive(true);
-                Pickeable pickeable = hitCollider.gameObject.GetComponent<Pickeable>();
+                Pickeable pickeable = hitColliders.gameObject.GetComponent<Pickeable>();
                 if (pickeable != null)
                 {
+                    infoPanel.transform.GetChild(0).gameObject.SetActive(true);
                     SetInfoText.SInstance.SetTextName(pickeable.ScriptableObject.nameItem);
                     SetInfoText.SInstance.SetTextType(gameObject.tag);
                     BeltController.SelectedBelt = GetComponent<BeltController>();
+                    hitColliders = null;
                 }
+
                 isHovering = true;
             }
         }
+
 
         if (!isHovering)
         {
